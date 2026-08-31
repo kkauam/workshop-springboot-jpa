14456969eaa9810e05ae2ccebd9165b60780a61e
kkauam <akauam109@gmail.com>
2026-08-31 16:30:22 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entity/User.java
+++ src/main/java/com/educandoweb/course/entity/User.java
@@ -1,12 +1,18 @@
 package com.educandoweb.course.entity;
+import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name= "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
--- src/main/java/com/educandoweb/course/resources/UsersResources.java
+++ src/main/java/com/educandoweb/course/resources/UsersResources.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/users")
 public class UsersResources {
-
-
     @GetMapping
     public ResponseEntity<User> findALL() {
         User user = new User(1L, "Maria", "Maria@gmail.com", "123456", "123456789");
